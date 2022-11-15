257c0a918b19a762e78dbc1824c058704cd38d5e
Muhammet Emin Göksu <115210524+goksugfb@users.noreply.github.com>
2022-11-15 23:15:28 +0300

    m to km
    Hello. An application is written that converts the value entered in this code string from meters to kilometers.

---
+++ metre_km.java
@@ -0,0 +1,18 @@
+/*
+Adim1: Basla
+Adim2: m degerini al.
+Adim3: km=m/1000
+Adim4: Yazdir=km
+Adim5: Bitir.
+*/
+package week1;
+import java.util.Scanner;
+public class metre_km {
+    public static void main(String[] args) {
+    Scanner input=new Scanner(System.in);
+        System.out.println("Lütfen çevirmek istediğiniz değeri (m) cinsinden giriniz:");
+        double m =input.nextDouble();
+        double km=m/1000;
+        System.out.println("Girdiğiniz uzunluğun km cinsinden değeri:" +km);
+    }  
+}
