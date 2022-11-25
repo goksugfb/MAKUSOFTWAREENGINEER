dc70e78105a4e132704016995e75ab069f2fd582
Muhammet Emin Göksu <115210524+goksugfb@users.noreply.github.com>
2022-11-25 23:35:46 +0300

    Update carpansiz_carpma.java

--- carpansiz_carpma.java
+++ carpansiz_carpma.java
@@ -1,5 +1,9 @@
 /*
-
+1.Adim: Başla
+2.Adim: Al m,n ve k=0
+3.Adim: for(i=1; i<=n; i++){k=k+m}
+4.Adim: Yazdir "m"
+5.Adim: Bitir
 */
 package makusoftwareengineer;
 import java.util.Scanner;
@@ -11,7 +15,7 @@ public class carpansiz_carpma {
         System.out.println("Lütfen çarpmak istediğiniz ikinci sayıyı giriniz:");
         int m=input.nextInt();
         int k=0;
-        for (int i = 1; i <=n; i++) {
+        for (int i = 1; i<=n; i++) {
             k=k+m;
         }
         System.out.println("Çarpım sonucunuz: "+k);
